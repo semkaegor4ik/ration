cc1075457aef899c3ec0c863f5f1bd493e89b659
26790 <267902.egor@gmail.com>
2021-01-10 02:37:06 +0300

    work +-

--- src/main/java/com/university/oop/diet/model/User.java
+++ src/main/java/com/university/oop/diet/model/User.java
@@ -35,6 +35,11 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    public boolean isAdmin()
+    {
+        return roles.contains(Role.ADMIN);
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles;
