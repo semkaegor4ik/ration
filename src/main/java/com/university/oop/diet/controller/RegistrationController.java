d5c6e420401e100f9e51b8f06d1f464766903078
26790 <267902.egor@gmail.com>
2021-01-10 03:55:46 +0300

    work +-

--- src/main/java/com/university/oop/diet/controller/RegistrationController.java
+++ src/main/java/com/university/oop/diet/controller/RegistrationController.java
@@ -4,9 +4,12 @@ import com.university.oop.diet.model.Role;
 import com.university.oop.diet.model.User;
 import com.university.oop.diet.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collections;
 import java.util.Map;
@@ -41,6 +44,21 @@ public class RegistrationController {
         user.setRoles(Collections.singleton(Role.USER));
         userRepo.save(user);
         return "redirect:/login";
+    }
+
+
+    @PutMapping("/changeInfo")
+    public String changeUser(User user, Map<String, Object> model)
+    {
+        User myUser = userRepo.getOne(((User)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
+        myUser.setActivity(user.getActivity());
+        myUser.setAge(user.getAge());
+        myUser.setEmail(user.getEmail());
+        myUser.setGender(user.getGender());
+        myUser.setHeight(user.getHeight());
+        myUser.setWeight(user.getWeight());
+        myUser.setPassword(user.getPassword());
+        return "redirect:/registration";
     }
 
 }
--- src/main/java/com/university/oop/diet/model/Gender.java
+++ src/main/java/com/university/oop/diet/model/Gender.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public enum Gender {
     MALE(66, 13.7, 5, 6.8),
-    FEMAE(655, 9.6, 1.8, 4.7);
+    FEMALE(655, 9.6, 1.8, 4.7);
     private final double startCoefficient;
     private final double weightCoefficient;
     private final double heightCoefficient;
